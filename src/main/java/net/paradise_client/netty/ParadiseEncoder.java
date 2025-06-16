be4eb3e0ca9d83bb4808684ff9205e6859519a96
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-06-16 20:09:12 +0530

    fixed minor bug with paradise encoder

--- src/main/java/net/paradise_client/netty/ParadiseEncoder.java
+++ src/main/java/net/paradise_client/netty/ParadiseEncoder.java
@@ -5,6 +5,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 import net.md_5.bungee.protocol.DefinedPacket;
 import net.md_5.bungee.protocol.Protocol;
+import net.minecraft.SharedConstants;
 import net.paradise_client.Helper;
 import net.paradise_client.ParadiseClient;
 import net.paradise_client.inject.accessor.DirectionDataAccessor;
@@ -12,7 +13,7 @@ import net.paradise_client.inject.accessor.DirectionDataAccessor;
 public class ParadiseEncoder extends MessageToByteEncoder<DefinedPacket> {
     protected void encode(ChannelHandlerContext ctx, DefinedPacket msg, ByteBuf out) throws Exception {
         Protocol protocol = Helper.getBungeeProtocolForCurrentPhase();
-        int protocolVersion = ParadiseClient.NETWORK_CONFIGURATION.protocolVersion;
+        int protocolVersion = SharedConstants.getProtocolVersion();
 
         int packetId = ((DirectionDataAccessor) (Object) protocol.TO_SERVER)
                 .paradiseClient$getId(msg.getClass(), protocolVersion);
