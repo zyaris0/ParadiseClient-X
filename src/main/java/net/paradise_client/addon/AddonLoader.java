e5ff51f48af9a82351450d552e62ed646d8f9344
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-06-16 22:43:38 +0530

    fix: initialize name and authors in addon

--- src/main/java/net/paradise_client/addon/AddonLoader.java
+++ src/main/java/net/paradise_client/addon/AddonLoader.java
@@ -1,6 +1,7 @@
 package net.paradise_client.addon;
 
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.metadata.Person;
 import net.paradise_client.Constants;
 
 import java.util.ArrayList;
@@ -10,12 +11,22 @@ public class AddonLoader {
     private static final List<ParadiseAddon> loadedAddons = new ArrayList<>();
 
     public static void loadAddons() {
-        FabricLoader.getInstance().getEntrypoints("paradise", ParadiseAddon.class)
-                .forEach(addon -> {
-            loadedAddons.add(addon);
-            addon.onInitialize();
-            Constants.LOGGER.info("Loaded addon: {}", addon.getClass().getName());
-        });
+        FabricLoader.getInstance().getEntrypointContainers("paradise", ParadiseAddon.class)
+                .forEach(container -> {
+                    try {
+                        ParadiseAddon addon = container.getEntrypoint();
+                        addon.name = container.getProvider().getMetadata().getName();
+                        addon.authors = container.getProvider().getMetadata().getAuthors()
+                                .stream()
+                                .map(Person::getName)
+                                .toArray(String[]::new);
+                        loadedAddons.add(addon);
+                        addon.onInitialize();
+                        Constants.LOGGER.info("Loaded addon: {}", addon.name);
+                    } catch (Throwable e) {
+                        Constants.LOGGER.error("Failed to load addon: {}", container.getProvider().getMetadata().getName(), e);
+                    }
+                });
     }
 
     public static List<ParadiseAddon> getLoadedAddons() {
