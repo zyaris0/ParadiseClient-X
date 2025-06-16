8dcd9213e2532cd266e726bbba3b8e0892a8c743
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-06-16 22:23:29 +0530

    refactored: addon loading

--- src/main/java/net/paradise_client/addon/AddonLoader.java
+++ src/main/java/net/paradise_client/addon/AddonLoader.java
@@ -1,8 +1,6 @@
 package net.paradise_client.addon;
 
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.metadata.CustomValue;
-import net.fabricmc.loader.api.metadata.Person;
 import net.paradise_client.Constants;
 
 import java.util.ArrayList;
@@ -12,37 +10,11 @@ public class AddonLoader {
     private static final List<ParadiseAddon> loadedAddons = new ArrayList<>();
 
     public static void loadAddons() {
-        FabricLoader.getInstance().getAllMods().forEach(mod -> {
-            var metadata = mod.getMetadata();
-            var customValue = metadata.getCustomValue("paradise");
-
-            if (customValue != null && customValue.getType() == CustomValue.CvType.OBJECT) {
-                var obj = customValue.getAsObject();
-
-                var entrypointValue = obj.get("entrypoint");
-
-                if (entrypointValue != null && entrypointValue.getType() == CustomValue.CvType.STRING) {
-                    String mainClass = entrypointValue.getAsString();
-
-                    try {
-                        Class<?> clazz = Class.forName(mainClass);
-                        if (!ParadiseAddon.class.isAssignableFrom(clazz)) return;
-
-                        ParadiseAddon addon = (ParadiseAddon) clazz.getDeclaredConstructor().newInstance();
-                        addon.name = metadata.getName();
-                        addon.authors = metadata.getAuthors().stream()
-                                .map(Person::getName)
-                                .toArray(String[]::new);
-
-                        loadedAddons.add(addon);
-                        addon.onInitialize();
-
-                        Constants.LOGGER.info("[ParadiseAddon] Loaded addon: {}", addon.name);
-                    } catch (Exception e) {
-                        Constants.LOGGER.error("Failed to load addon from {}", mod.getMetadata().getId(), e);
-                    }
-                }
-            }
+        FabricLoader.getInstance().getEntrypoints("paradise", ParadiseAddon.class)
+                .forEach(addon -> {
+            loadedAddons.add(addon);
+            addon.onInitialize();
+            Constants.LOGGER.info("Loaded addon: {}", addon.getClass().getName());
         });
     }
 
