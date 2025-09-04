b18fd3467890af2d0204d604257ed25f0a0a83cf
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-09-04 13:19:17 +0530

    feat: LoginEvent

---
+++ src/main/java/net/paradise_client/event/network/LoginEvent.java
@@ -0,0 +1,38 @@
+package net.paradise_client.event.network;
+
+import com.mojang.authlib.GameProfile;
+import io.github.spigotrce.eventbus.event.*;
+
+public class LoginEvent extends Event implements Cancellable {
+  private boolean disableHandler;
+  private GameProfile profile;
+
+  public LoginEvent(GameProfile profile) {
+    this.disableHandler = false;
+    this.profile = profile;
+  }
+
+  public boolean isDisableHandler() {
+    return disableHandler;
+  }
+
+  public void setDisableHandler(boolean disableHandler) {
+    this.disableHandler = disableHandler;
+  }
+
+  public GameProfile getProfile() {
+    return profile;
+  }
+
+  public void setProfile(GameProfile profile) {
+    this.profile = profile;
+  }
+
+  @Override public boolean isCancel() {
+    return isDisableHandler();
+  }
+
+  @Override public void setCancel(boolean b) {
+    setDisableHandler(b);
+  }
+}
---
+++ src/main/java/net/paradise_client/inject/mixin/network/handler/ClientLoginNetworkHandlerMixin.java
@@ -0,0 +1,21 @@
+package net.paradise_client.inject.mixin.network.handler;
+
+import net.minecraft.client.network.*;
+import net.minecraft.network.packet.s2c.login.LoginSuccessS2CPacket;
+import net.paradise_client.ParadiseClient;
+import net.paradise_client.event.network.LoginEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientLoginNetworkHandler.class)
+public class ClientLoginNetworkHandlerMixin {
+  @Inject(method = "onSuccess", at = @At("HEAD"), cancellable = true)
+  public void onSucess(LoginSuccessS2CPacket packet, CallbackInfo ci) throws Exception {
+    LoginEvent event = new LoginEvent(packet.profile());
+    ParadiseClient.EVENT_MANAGER.fireEvent(event);
+    if (event.isDisableHandler()) {
+      ci.cancel();
+    }
+  }
+}
