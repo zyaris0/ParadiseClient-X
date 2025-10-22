33840534992b6d9506043ee5dbe116a8b1495e17
Zyaris <164185653+zyaris0@users.noreply.github.com>
2025-10-22 07:18:33 +0530

    Update OptionUtil.java

--- src/main/java/net/paradise_client/utils/OptionUtil.java
+++ src/main/java/net/paradise_client/utils/OptionUtil.java
@@ -1,4 +1,5 @@
 package net.paradide_client.utils;
+import net.paradise_client.utils.OptionType;
 
 public record OptionUtil(String name, OptionType type, String[] args) {
     public OptionUtil(String name, OptionType type) {
